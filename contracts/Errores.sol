d2d0173503169aa92fdf2ebd28f5a055048a59dc
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-01-18 23:24:02 -0600

    Errores

---
+++ contracts/Errores.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Modificadores {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function suma(uint numero1, uint numero2) public view esOwner() returns (uint) {
+        return numero1 + numero2;
+    }
+
+    modifier esOwner() {
+        require(
+            owner == msg.sender,
+            "El usuario no es el creador del contrato"
+        );
+        _;
+    }
+}
